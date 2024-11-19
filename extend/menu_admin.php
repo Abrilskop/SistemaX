2273583fd5f75d04a911aad7f28d70e2fafe966c
Abrilskop <uberpot.ia@gmail.com>
2024-11-19 12:16:14 -0500

    feat: add extend and components

---
+++ extend/menu_admin.php
@@ -0,0 +1,33 @@
+<nav>
+  <a href="" data-activates="menu" class="button-collpase" ><i class="material-icons">menu</i></a>
+</nav>
+<ul id="menu" class="side-nav fixed" >
+  <li>
+    <div class="userView" >
+      <div class="background">
+        <img src="../img/logo.jpeg"  >
+      </div>
+      <a href="" ><img src="../img/gia.jpeg" class="circle" alt=""></a>
+      <br>
+      <br>
+      <br>
+      <a href="" class="black-text" >Gianella Alexandra</a>
+      <a href="" class="black-text">uberpot.ia@gmail.com</a>
+    </div>
+  </li>
+  <!-- implementacion guia -->
+  <li><a href="./login.html"><i class="material-icons">home</i> SALIR</a></li>
+  <li><div class="divider" ></div></li>
+  <li><a href="../usuarios/index.php"><i class="material-icons">beenhere</i>ALTA USUARIOS</a></li>
+  <li><div class="divider" ></div></li>
+  <li><a href="../productos/index.php"><i class="material-icons">beenhere</i>ALTA PRODUCTOS</a></li>
+  <li><div class="divider" ></div></li>
+  <li><a href="../clientes/index.php"><i class="material-icons">beenhere</i>ALTA CLIENTES</a></li>
+  <li><div class="divider" ></div></li>
+  <li><a href="../ventas/index.php"><i class="material-icons">beenhere</i>ALTA VENTAS</a></li>
+  <li><div class="divider" ></div></li>
+  <li><a href="../detalleventa/index.php"><i class="material-icons">beenhere</i>ALTA DETALLEVENTA</a></li>
+  <li><div class="divider" ></div></li>
+  <li><a href="salir.php"><i class="material-icons">close</i> Salir</a></li>
+  <li><div class="divider" ></div></li>
+</ul>
