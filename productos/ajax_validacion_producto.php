c59c2e24963c747205247a6a59a72a343dd4266d
Abrilskop <uberpot.ia@gmail.com>
2024-11-20 12:40:12 -0500

    feat: add productos

---
+++ productos/ajax_validacion_producto.php
@@ -0,0 +1,16 @@
+<?php
+    include "../conexion/conexion.php";
+
+    $producto = $con->real_escape_string($_POST['producto']);
+
+    $sel = $con->query("SELECT idProducto FROM productos WHERE Nombre='$producto'");
+    $row = mysqli_num_rows($sel);
+
+    if ($row != 0) {
+        echo "<label style='color:red;'>El nombre del producto ya existe</label>";
+    } else {
+        echo "<label style='color:green;'>El nombre del producto está disponible</label>";
+    }
+
+    $con->close();
+?>
